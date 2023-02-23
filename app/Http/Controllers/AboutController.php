c068ea6a2bc2bbdb4471b298c353376bf5a7d23a
BrianMohamadSafiudin <brianms2004@gmail.com>
2023-02-23 13:50:17 +0700

    05_praktikum_web_lanjut_dua

---
+++ app/Http/Controllers/AboutController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AboutController
+{
+    public function __invoke() {
+        return "2141720133 Brian Mohamad Safiudin" ;
+    }
+}
